12f617078442d991f6c7d3c1483111862296f7ee
roushan-sheik <roushansheik@gmail.com>
2024-08-03 19:51:31 +0600

    added: reuseable classes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,19 @@
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      {/* left box  */}
+      <div>
+        <img className="" src="../../public/logo.png" alt="" />
+        <h2 className="text_black_pri">Sign In To Your Account</h2>
+        <p>
+          elcome Back! By click the sign up button, you're agree to Zenitood
+          Terms and Service and acknlowledge the Privacy and Policy
+        </p>
+      </div>
+      {/* right box  */}
+      <div>right box</div>
+    </div>
+  );
 };
 
 export default Login;
