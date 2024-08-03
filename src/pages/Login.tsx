5d9d42b105a514bb85867ef24361e90090c41118
roushan-sheik <roushansheik@gmail.com>
2024-08-03 20:06:49 +0600

    form title added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,25 @@
 const Login = () => {
   return (
-    <div>
+    <div className="main_  flex justify-between items-center flex-col-reverse lg:flex-row ">
       {/* left box  */}
-      <div>
+      <div className="basis-[50%]">
         <img className="" src="../../public/logo.png" alt="" />
-        <h2 className="text_black_pri">Sign In To Your Account</h2>
-        <p>
-          elcome Back! By click the sign up button, you're agree to Zenitood
-          Terms and Service and acknlowledge the Privacy and Policy
-        </p>
+        <div>
+          <h2 className="text_black_pri lg:text-3xl text-[28px] text-center lg:text-start font-semibold">
+            Sign In To Your Account
+          </h2>
+          <p>
+            elcome Back! By click the sign up button, you are agree to Zenitood
+            Terms and Service and acknlowledge the{" "}
+            <a href="#" className="underline text_brand">
+              Privacy and Policy
+            </a>
+          </p>
+        </div>
+        {/* form  */}
       </div>
       {/* right box  */}
-      <div>right box</div>
+      <div className="basis-[50%]">right box</div>
     </div>
   );
 };
